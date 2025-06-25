04e5001e0fb5ee4c5ce3797605ff3b9930ff1bf7
mohcine-ahadjane <mohcine-ahadjane@gmail.com>
2025-06-25 22:51:44 +0100

    Adding JWT/OAuth2 + conternairization

--- task-backend/src/main/java/com/example/task_backend/security/JwtUtil.java
+++ task-backend/src/main/java/com/example/task_backend/security/JwtUtil.java
@@ -3,6 +3,7 @@ package com.example.task_backend.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -14,7 +15,8 @@ import io.jsonwebtoken.*;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "30bac526790dc49a3d12b115ad4059888e80530b9265a6fbf2395289a56b2a94";
+    @Value("${app.jwt.SECRET_KEY}")
+    private String SECRET_KEY;
 
     private final long EXPIRATION_TIME = 86400000;
     private Key getSigningKey() {
